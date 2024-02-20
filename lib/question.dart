946e07ad5b528c612151e0e68c9647fbb6e8bd28
adithyankkichu <160300759+adithyankkichu@users.noreply.github.com>
2024-02-20 16:44:52 +0530

    ui-v1

--- lib/question.dart
+++ lib/question.dart
@@ -12,7 +12,39 @@ class _QuestionsState extends State<Questions> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Text('?'),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Google was orginally called backrub',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+            )),
+            SizedBox(height: 60),
+            TextButton(onPressed: (){},
+                style: (TextButton.styleFrom(
+                  backgroundColor: Colors.green,
+                  fixedSize: Size(90, 40),
+                )),
+                child: Text('True',
+                style: TextStyle(
+                  color: Colors.white,
+                ))),
+            SizedBox(height: 20,),
+            TextButton(onPressed: (){},
+                style:TextButton.styleFrom(
+                  backgroundColor: Colors.red,
+                  fixedSize: Size(90, 40),
+                ),
+                child: Text('False',style: TextStyle(
+                  color: Colors.white,
+                ),),
+            ),
+          ],
+        ),
+      )
     );
   }
 }
